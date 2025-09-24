26278d378b9655dfc170ca670efe5d93ed4befee
abeliooo <stefanusabel29@gmail.com>
2025-09-24 22:38:08 +0700

    update package json
    update package json and fixing error link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,7 +13,7 @@
                     <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
                 </svg>
             </a>
-            <a href="https://linkedin.com/in/stefanus-fillio-4ab21a302" target="_blank" class="text-gray-500 hover:text-orange-600 transition-colors duration-300">
+            <a href="https://www.linkedin.com/in/stefanus-abel-fillio" target="_blank" class="text-gray-500 hover:text-orange-600 transition-colors duration-300">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
                     <rect width="4" height="12" x="2" y="9" />
